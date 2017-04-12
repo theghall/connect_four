f630d9653449a5d8b03b40d838204be056e3b0e2
theghall <guategary@gmail.com>
2017-04-12 16:05:54 -0400

    Add spec and implementation for take_turn

--- lib/connect_four_player.rb
+++ lib/connect_four_player.rb
@@ -3,15 +3,33 @@
 # 20170411	GH
 #
 class Player
-  attr_reader :name
+  attr_reader :name, :token
 
-  def initialize
+  def initialize(name, token)
+    @name = name
+    
+    @token = token
   end
 
-  def take_turn
+  def take_turn(board, judge)
+    loop do
+      print("Enter position: ")
+
+      pos = gets.chomp
+
+      ary = pos.split(',').map{ |e| e.to_i }
+
+      begin
+        token_placed = board.place_token(token, ary[0], ary[1]) if judge.valid_move?(ary[0], ary[1])
+      rescue ArgumenError => e
+        puts('Choose another position')
+      end
+
+      break if token_placed
+    end 
   end
 
   private
 
-  attr_writer :name
+  attr_writer :name, :token
 end
